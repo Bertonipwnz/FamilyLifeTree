beb27447793536f897f2dc6876fa5c5a1e4ef2be
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-08 20:58:35 +0700

    Пустой проект для vm и пустая страница treepage

---
+++ FamilyLifeTree.UWP/Views/Pages/TreePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace FamilyLifeTree.UWP.Views.Pages
+{
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// Страница с древом.
+    /// </summary>
+    public sealed partial class TreePage : Page
+    {
+        /// <summary>
+        /// Инициалазирует <see cref="TreePage"/>
+        /// </summary>
+        public TreePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
