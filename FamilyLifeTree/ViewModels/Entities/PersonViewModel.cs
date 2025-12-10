b4df878278e2989f171cac5b92625f0290e869f9
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-10 22:45:27 +0700

    (WIP) ItemsControl с расположением элементов

--- FamilyLifeTree/ViewModels/Entities/PersonViewModel.cs
+++ FamilyLifeTree/ViewModels/Entities/PersonViewModel.cs
@@ -1,19 +1,63 @@
 ﻿namespace FamilyLifeTree.ViewModels.Entities
 {
-	using FamilyLifeTree.Core.Models;
-	using Utils.Mvvm.ViewModels;
+    using FamilyLifeTree.Core.Models;
+    using Utils.Mvvm.ViewModels;
 
-	/// <summary>
-	/// Модель человека.
-	/// </summary>
-	public class PersonViewModel : AbstractEntityViewModel<Person>
+    /// <summary>
+    /// Модель представления человека для отображения на Canvas.
+    /// </summary>
+    public class PersonViewModel : AbstractEntityViewModel<Person>
 	{
+		/// <summary>
+		/// Модель домена.
+		/// </summary>
+		private readonly Person _person;
+
+		/// <summary>
+		/// Координата X на Canvas.
+		/// </summary>
+		private double _canvasLeft;
+
+		/// <summary>
+		/// Координата Y на Canvas.
+		/// </summary>
+		private double _canvasTop;
+
 		/// <summary>
 		/// Создает экземпляр <see cref="PersonViewModel"/>
 		/// </summary>
 		/// <param name="model">Модель.</param>
 		public PersonViewModel(Person model) : base(model)
 		{
+			_person = model;
+		}
+
+		/// <summary>
+		/// Доменная модель (используется для операций репозитория).
+		/// </summary>
+		public Person Model => _person;
+
+		/// <summary>
+		/// Отображаемое имя.
+		/// </summary>
+		public string DisplayName => _person.ShortName;
+
+		/// <summary>
+		/// Позиция элемента по оси X.
+		/// </summary>
+		public double CanvasLeft
+		{
+			get => _canvasLeft;
+			set => SetProperty(ref _canvasLeft, value);
+		}
+
+		/// <summary>
+		/// Позиция элемента по оси Y.
+		/// </summary>
+		public double CanvasTop
+		{
+			get => _canvasTop;
+			set => SetProperty(ref _canvasTop, value);
 		}
 	}
 }
--- FamilyLifeTree/ViewModels/Pages/TreePageViewModel.cs
+++ FamilyLifeTree/ViewModels/Pages/TreePageViewModel.cs
@@ -1,5 +1,12 @@
 ﻿namespace FamilyLifeTree.ViewModels.Pages
 {
+	using CommunityToolkit.Mvvm.Input;
+	using FamilyLifeTree.Core.Interfaces;
+	using FamilyLifeTree.Core.Models;
+	using FamilyLifeTree.ViewModels.Entities;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Threading.Tasks;
 	using Utils.Mvvm.ViewModels;
 
 	/// <summary>
@@ -8,11 +15,120 @@
 	public class TreePageViewModel : BasePageViewModel
 	{
 		/// <summary>
+		/// Отступ между карточками на Canvas.
+		/// </summary>
+		private const double NodeMargin = 12d;
+
+		/// <summary>
+		/// Ширина карточки.
+		/// </summary>
+		private const double NodeWidth = 120d;
+
+		/// <summary>
+		/// Высота карточки.
+		/// </summary>
+		private const double NodeHeight = 64d;
+
+		/// <summary>
+		/// Unit of Work для работы с БД.
+		/// </summary>
+		private readonly IUnitOfWork _unitOfWork;
+
+		/// <summary>
+		/// Текущий счётчик созданных персон.
+		/// </summary>
+		private int _personCounter = 1;
+
+		/// <summary>
+		/// Коллекция людей, отображаемых на Canvas.
+		/// </summary>
+		public ObservableCollection<PersonViewModel> Persons { get; } = new();
+
+		/// <summary>
+		/// Команда добавления человека.
+		/// </summary>
+		public IAsyncRelayCommand AddPersonCommand { get; }
+
+		/// <summary>
+		/// Команда удаления человека.
+		/// </summary>
+		public IAsyncRelayCommand<PersonViewModel> RemovePersonCommand { get; }
+
+		/// <summary>
 		/// Создает экземпляр <see cref="TreePageViewModel"/>
 		/// </summary>
-		public TreePageViewModel()
+		public TreePageViewModel(IUnitOfWork unitOfWork)
 		{
-			//TODO: Команды и логика.
+			_unitOfWork = unitOfWork;
+
+			AddPersonCommand = new AsyncRelayCommand(AddPersonAsync);
+			RemovePersonCommand = new AsyncRelayCommand<PersonViewModel>(RemovePersonAsync);
+		}
+
+		/// <inheritdoc/>
+		public override void OnNavigatedTo(object param = null)
+		{
+			_ = LoadPersonsAsync();
+		}
+
+		private async Task LoadPersonsAsync()
+		{
+			var persons = await _unitOfWork.Persons.GetAllAsync();
+
+			Persons.Clear();
+
+			int index = 0;
+			foreach (var person in persons)
+			{
+				var vm = CreateVm(person, index);
+				Persons.Add(vm);
+				index++;
+			}
+
+			_personCounter = Persons.Count + 1;
+		}
+
+		private async Task AddPersonAsync()
+		{
+			var person = new Person
+			{
+				FirstName = "Person",
+				LastName = _personCounter.ToString()
+			};
+
+			await _unitOfWork.Persons.AddAsync(person);
+			await _unitOfWork.CompleteAsync();
+
+			var vm = CreateVm(person, Persons.Count);
+			Persons.Add(vm);
+
+			_personCounter++;
+		}
+
+		private async Task RemovePersonAsync(PersonViewModel personViewModel)
+		{
+			if (personViewModel == null)
+				return;
+
+			if (!Persons.Contains(personViewModel))
+				return;
+
+			_unitOfWork.Persons.Remove(personViewModel.Model);
+			await _unitOfWork.CompleteAsync();
+
+			Persons.Remove(personViewModel);
+		}
+
+		private PersonViewModel CreateVm(Person person, int index)
+		{
+			var col = index % 5;
+			var row = index / 5;
+
+			return new PersonViewModel(person)
+			{
+				CanvasLeft = col * (NodeWidth + NodeMargin),
+				CanvasTop = row * (NodeHeight + NodeMargin)
+			};
 		}
 	}
 }
