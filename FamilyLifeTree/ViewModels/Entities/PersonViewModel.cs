540c16c76b3749009f702511ba987aa996f88a65
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-10 22:20:56 +0700

    Добавление базы view

---
+++ FamilyLifeTree/ViewModels/Entities/PersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FamilyLifeTree.ViewModels.Entities
+{
+	using FamilyLifeTree.Core.Models;
+	using Utils.Mvvm.ViewModels;
+
+	/// <summary>
+	/// Модель человека.
+	/// </summary>
+	public class PersonViewModel : AbstractEntityViewModel<Person>
+	{
+		/// <summary>
+		/// Создает экземпляр <see cref="PersonViewModel"/>
+		/// </summary>
+		/// <param name="model">Модель.</param>
+		public PersonViewModel(Person model) : base(model)
+		{
+		}
+	}
+}
