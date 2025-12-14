d284b64b435e58c88f834a8c844cb5d1ebcb8e16
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-14 18:51:27 +0700

    Пустышки для стартовой страницы

---
+++ FamilyLifeTree/ViewModels/Pages/StartPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FamilyLifeTree.ViewModels.Pages
+{
+    using FamilyLifeTree.Core.Interfaces;
+    using Utils.Mvvm.ViewModels;
+
+	/// <summary>
+	/// Модель представления стартовой страницы.
+	/// </summary>
+	public class StartPageViewModel : BasePageViewModel
+	{
+		public StartPageViewModel(IUnitOfWork unitOfWork) { }
+	}
+}
