fba81a431946c06176d3755024b1b3a7e1988bf6
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-16 22:12:41 +0700

    Добавление сервиса локализаций

---
+++ FamilyLifeTree.Core/Interfaces/ILocalizationService.cs
@@ -0,0 +1,17 @@
+﻿namespace FamilyLifeTree.Core.Interfaces
+{
+#nullable enable
+
+	/// <summary>
+	/// Интерфейс сервиса локализации.
+	/// </summary>
+	public interface ILocalizationService
+	{
+		/// <summary>
+		/// Получает локализованную строку.
+		/// </summary>
+		/// <param name="resourcesKey">Ключ ресурса локализации.</param>
+		/// <param name="resourcesName">Имя ресурса откуда берется локализация.</param>
+		public string GetLocalizationString(string resourcesKey, string? resourcesName = null);
+	}
+}
---
+++ FamilyLifeTree.UWP/Services/LocalizationService.cs
@@ -0,0 +1,38 @@
+﻿namespace FamilyLifeTree.UWP.Services
+{
+    using FamilyLifeTree.Core.Interfaces;
+    using Serilog;
+    using System;
+    using Utils.Logger;
+    using Windows.ApplicationModel.Resources;
+
+    /// <summary>
+    /// Сервис локализации.
+    /// </summary>
+    public class LocalizationService : ILocalizationService
+	{
+		#region Private Fields
+
+		/// <summary>
+		/// Дефолтный ресурс лоадер (Resources.resw)
+		/// </summary>
+		private static readonly ResourceLoader _defaultResourceLoader = ResourceLoader.GetForCurrentView();
+
+		/// <summary>
+		/// Логгер.
+		/// </summary>
+		private static readonly ILogger _logger = LogService.GetCurrentLogger();
+
+		#endregion
+
+		#region Public Methods
+
+		/// <inheritdoc/>
+		public string GetLocalizationString(string resourcesKey, string? resourcesName = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
