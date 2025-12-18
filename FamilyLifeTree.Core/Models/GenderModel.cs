e5fca968b7d2590439d5a4110657ae26c5761111
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-18 22:24:22 +0700

    Модель гендера

---
+++ FamilyLifeTree.Core/Models/GenderModel.cs
@@ -0,0 +1,25 @@
+﻿namespace FamilyLifeTree.Core.Models
+{
+    using FamilyLifeTree.Core.Enums;
+
+    //TODO: Посмотреть необходимость public класса.
+    /// <summary>
+    /// Модель гендера.
+    /// </summary>
+    public class GenderModel
+	{
+		#region Public Properties
+
+        /// <summary>
+        /// Перечисление гендера.
+        /// </summary>
+		public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Путь к иконке.
+        /// </summary>
+        public string IconPath { get; set; } = string.Empty;
+
+		#endregion
+	}
+}
