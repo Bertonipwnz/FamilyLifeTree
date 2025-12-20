40f4549a71e2d05b207d14f320dca1c1907bcbc3
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-20 23:32:49 +0700

    Интерфейс сервиса сущностей

---
+++ Utils/Interfaces/IEntityService.cs
@@ -0,0 +1,26 @@
+﻿namespace Utils.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Интерфейс сервиса сущности.
+    /// </summary>
+    /// <typeparam name="M">Модель.</typeparam>
+    /// <typeparam name="VM">Модель представления.</typeparam>
+    public interface IEntityService<M,VM>
+    {
+        /// <summary>
+        /// Создает модель
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public VM CreateVM(M model);
+
+        /// <summary>
+        /// Метод получения поделей.
+        /// </summary>
+        public async Task<List<M>> GetModelsAsync();
+
+    }
+}
