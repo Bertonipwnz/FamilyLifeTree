28d53bfc69f11d1057b0814d9e41b61a0122c5cb
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-12-21 20:35:17 +0700

    Структура сервиса гендеров

---
+++ FamilyLifeTree.Core/Interfaces/IGenderService.cs
@@ -0,0 +1,13 @@
+﻿namespace FamilyLifeTree.Core.Interfaces
+{
+	using Utils.Interfaces;
+
+	/// <summary>
+	/// Интерфейса сервиса гендеров.
+	/// </summary>
+	/// <typeparam name="M">Модель.</typeparam>
+	/// <typeparam name="VM">Модель представления.</typeparam>
+	public interface IGenderService<M, VM> : IEntityService<M, VM>, IAsyncInitializable
+	{
+	}
+}
---
+++ FamilyLifeTree/Services/GenderService.cs
@@ -0,0 +1,29 @@
+﻿namespace FamilyLifeTree.Services
+{
+    using FamilyLifeTree.Core.Interfaces;
+    using FamilyLifeTree.Core.Models;
+    using FamilyLifeTree.ViewModels.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class GenderService : IGenderService<GenderModel, GenderViewModel>
+    {
+        public IEnumerable<GenderViewModel> ViewModels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsInitialized => throw new NotImplementedException();
+
+        public event EventHandler Initialized;
+
+        public GenderViewModel CreateVM(GenderModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
